8dc5355d80b5f7bcb283110e2290c2180ac7b0e0
Six <84689659+SixofClubsss@users.noreply.github.com>
2024-04-07 22:02:27 +0000

    mod to https://github.com/dReam-dApps/dReams/commit/7d61c9ead1fe7cc3788d610380103911e6efa0f6

--- prediction/start.go
+++ prediction/start.go
@@ -28,7 +28,7 @@ const (
 	appID   = "dreamdapps.io.dprediction"
 )
 
-var version = semver.MustParse("0.3.1-dev.7")
+var version = semver.MustParse("0.3.1-dev.8")
 var gnomon = gnomes.NewGnomes()
 
 // Check prediction package version
