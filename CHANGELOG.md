7ab88efd4f3d911b527693d5e1d95cd30782f1f7
Six <84689659+SixofClubsss@users.noreply.github.com>
2024-07-15 18:48:51 +0000

    mod to https://github.com/dReam-dApps/dReams/commit/cdb4bee095b1ea143fef45c79205cb9d293dec24

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,7 @@ This file lists the changes to dPrediction repo with each version.
 
 ### Changed
 
-* Fyne 2.4.5
+* Fyne 2.5.0
 * dReams 0.12.0
 * Update StartApp with upstream functions
 * `dreams` UpdateText and SetUpdate for labels
--- prediction/start.go
+++ prediction/start.go
@@ -28,7 +28,7 @@ const (
 	appID   = "dreamdapps.io.dprediction"
 )
 
-var version = semver.MustParse("0.3.1-dev.11")
+var version = semver.MustParse("0.3.1-dev.12")
 var gnomon = gnomes.NewGnomes()
 
 // Check prediction package version
